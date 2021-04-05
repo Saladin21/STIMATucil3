7181aa34d06266130d53b33b234538415c4447a1
Saladin21 <ahmadsaladin21@gmail.com>
2021-04-05 16:14:57 +0700

    bisa tambah edge

--- app.py
+++ app.py
@@ -15,12 +15,32 @@ matrix = []
 def index():
     return render_template('home.html')
 
+@app.route('/sendGraph', methods =['POST'])
+def sendGraph():
+    while (matrix):
+        matrix.pop(0)
+    for j in range(len(graph)):
+        matrix.append([0 for i in range(len(graph))])
+
+    #matrix = [[0 for i in range (len(graph))] for j in range (len(graph))]
+    print(matrix)
+    return render_template('addEdge.html', graph=graph)
+
 #python route
 @app.route('/this-route', methods=['GET', 'POST'])
 def thisRoute():
     information = json.loads(request.data )
     graph[information[0]] = [information[1]["lat"], information[1]["lng"]]
-    //print(graph)
+    #print(graph)
+    return "1"
+
+@app.route('/kirim_matriks', methods=['GET', 'POST'])
+def dapetMatriks():
+    info = json.loads(request.data)
+    print(info)
+    print(matrix)
+    matrix[int(info[0])-1][int(info[1])-1] = 1
+    matrix[int(info[1])-1][int(info[0])-1] = 1
     return "1"
 
 if __name__ == "__main__":
